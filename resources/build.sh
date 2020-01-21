1a4fd43c2ba3c58319105ae78d0edc005208e300
Brian Baldino <bbaldino@gmail.com>
2020-01-21 13:24:07 -0800

    add docs

--- resources/build.sh
+++ resources/build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# Given a regex, return the most recent git tag which matches
 function getLatestTagMatching() {
   regex=$1
   echo $(git describe --match "$regex" --abbrev=0 --tags `git rev-list --tags --max-count=1`)
 }
 
+# Given the variable (maven property) used to hold a module's version, return the
+# version in the form of Major.Minor (no '-SNAPSHOT')
 function getMajorMinorVersionOfModule() {
   module_version_variable_name=$1
   current_version=$(mvn help:evaluate -Dexpression="$module_version_variable_name" -q -DforceStdout)
   echo "${current_version/-SNAPSHOT/}"
 }
 
+# Arguments:
+# 1) module_name: A friendly name for the module.  Used for logging
+# 2) module_version_variable_name: The name of the maven property used to hold this module's version
+# 3) module_tag_regex: A regex which describes the git tags used for this module
+# 4) module_dir: The directory in which the module is held.  Used to count how many changes have
+#                been made in this module since the last tag
 function setVersionForModule() {
   module_name=$1
   module_version_variable_name=$2
@@ -23,7 +32,7 @@ function setVersionForModule() {
   most_recent_tag=$(getLatestTagMatching "$module_tag_regex")
   echo "Most recent $module_name tag is $most_recent_tag"
 
-  # Get the hash of the commit corresponding to the most recent RTP change to be used in the version
+  # Get the hash of the commit corresponding to the most recent change to be used in the version
   most_recent_change_hash_short=$(git log --format=%h --max-count=1 $most_recent_tag..HEAD $module_dir)
   echo "Most recent commit in $module_name is $most_recent_change_hash_short"
   # xargs is used to trim whitespace from the result
